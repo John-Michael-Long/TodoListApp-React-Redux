7191b2e1914f0cd56f1b73475c0bb0a3a4ed178c
John M. Long <36753682+John-Michael-Long@users.noreply.github.com>
2018-11-24 12:45:53 -0500

    clean up code

--- client/src/actions/actions.js
+++ client/src/actions/actions.js
@@ -1,17 +1,20 @@
 import * as actionTypes from './actionTypes';
 
+let nextTodoId = 0;
+
 export const addTodo = text => ({
   type: actionTypes.ADD_TODO,
+  id: nextTodoId++,
   text  
 });
 
-export const toggleTodo = index => ({
+export const toggleTodo = id => ({
   type: actionTypes.TOGGLE_TODO,
-  index
+  id
 });
 
-export const setVisibilityFilter = filter => {
+export const setVisibilityFilter = filter => ({
   type: actionTypes.SET_VISIBILITY_FILTER,
   filter
-}
+});
 
