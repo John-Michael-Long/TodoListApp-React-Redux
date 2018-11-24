6d02c6202dae6b62983d8c54cfbb9d5c6cbfb7ff
John M. Long <36753682+John-Michael-Long@users.noreply.github.com>
2018-11-24 12:48:39 -0500

    add tests code for addTodo, toggleTodo, and setVisibilityFilter

--- client/src/index.js
+++ client/src/index.js
@@ -1,20 +1,45 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import style from './styles/main.css';
+import { render } from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
 import App from './components/App.jsx';
-//import other components
 
-ReactDOM.render(<App />, document.getElementById('app'));
+import style from './styles/main.css';
+import todoApp from './reducers/todosReducer.js';
 
-//WHEN USING REDUX UNCOMMENT BELOW:
+import {
+  addTodo,
+  toggleTodo,
+  setVisibilityFilter
+} from './actions/actions';
 
-// import store from './store/store.js';
-// import { render } from 'react-dom';
-// import { Provider } from 'react-redux';
+import * as actionTypes from './actions/actionTypes';
 
-// ReactDOM.render(
-//   <Provider store={store}>
-//     <App API_KEY={YOUTUBE_API_KEY} searchYouTube={searchYouTube} />
-//   </Provider>,
-//   document.getElementById('app')
-// );
+const store = createStore(todoApp);
+
+// Log the initial state
+console.log( store.getState() )
+// Every time the state changes, log it
+// Note that subscribe() returns a function for unregistering the listener
+const unsubscribe = store.subscribe(() => console.log(store.getState()))
+
+// Dispatch some actions
+store.dispatch(addTodo('Learn about actions'))
+store.dispatch(addTodo('Learn about reducers'))
+store.dispatch(addTodo('Learn about store'))
+store.dispatch(toggleTodo(0))
+store.dispatch(toggleTodo(1))
+store.dispatch(setVisibilityFilter(actionTypes.VisibilityFilters.SHOW_COMPLETED))
+
+// Stop listening to state updates
+unsubscribe()
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('app')
+)
+
+
